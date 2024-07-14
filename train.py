92f7147a98eb26aa5729a616b068576b65916772
Rvarsha1503 <varsharacharla1503@gmail.com>
2024-07-14 17:41:16 +0530

    train.py

---
+++ train.py
@@ -0,0 +1,59 @@
+import tensorflow as tf
+from tensorflow.keras.applications import VGG16
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Dense, Flatten, Dropout
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.callbacks import EarlyStopping, ReduceLROnPlateau
+
+# Data augmentation and preprocessing
+train_datagen = ImageDataGenerator(
+    rescale=1./255,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True,
+    rotation_range=30,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    brightness_range=[0.8,1.2]
+)
+
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+training_set = train_datagen.flow_from_directory(
+    'xray/train',
+    target_size=(224, 224),
+    batch_size=32,
+    class_mode='categorical'
+)
+
+test_set = test_datagen.flow_from_directory(
+    'xray/test',
+    target_size=(224, 224),
+    batch_size=32,
+    class_mode='categorical'
+)
+
+# Model architecture
+base_model = VGG16(input_shape=(224, 224, 3), include_top=False, weights='imagenet')
+for layer in base_model.layers:
+    layer.trainable = False
+
+x = Flatten()(base_model.output)
+x = Dense(128, activation='relu')(x)
+x = Dropout(0.5)(x)
+x = Dense(2, activation='softmax')(x)  # Adjust the number of classes accordingly
+
+model = Model(inputs=base_model.input, outputs=x)
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+# Callback initialization
+early_stopping = EarlyStopping(monitor='val_loss', patience=5, restore_best_weights=True)
+reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.2, patience=3, min_lr=1e-5)
+
+# Training
+history = model.fit(
+    training_set,
+    validation_data=test_set,
+    epochs=7, #change the epochs number accordingly, for better accuracy
+    callbacks=[early_stopping, reduce_lr]
+)
